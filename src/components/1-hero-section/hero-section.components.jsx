f2af0e4d1e86a0d12923eb1e134259aff431f4a1
ornops <joshuaornopia@gmail.com>
2024-05-25 17:50:14 +0800

    Started working on Hero Section

--- src/components/1-hero-section/hero-section.components.jsx
+++ src/components/1-hero-section/hero-section.components.jsx
@@ -1,10 +1,19 @@
 import './hero-section.styles.css'
+import HeroImage from '../../assets/hero-image.jpg'
 
 const HeroSection = () =>{
 
     return(
         <div className="section-container hero-section">
-            <h1>Hero Section</h1>   
+            <div className="left-side-container">
+                <img src={HeroImage} alt="HeroImage" />
+            </div> 
+            <div className="right-side-container">
+                <h1 className='my-name'>Joshua Ornopia</h1>
+                <div className="position-title">Frontend Engineer | QA Engineer</div>
+                <button className="contact-button">Reach Out</button>
+
+            </div>
         </div>
     )
 }
