d67cbec4b4f27901b16c93c3260c83bae3180612
ornops <joshuaornopia@gmail.com>
2024-06-01 22:52:57 +0800

    Updated About section

--- src/components/2-about-section/about-section.components.jsx
+++ src/components/2-about-section/about-section.components.jsx
@@ -1,10 +1,18 @@
 import './about-section.styles.css'
+import AboutImage from '../../assets/about-image.jpg'
 
 const AboutSection = () =>{
 
     return(
         <div className="section-container about-section">
-            <h1>About Section</h1>
+            <div className="about-left-section">
+                <h1>About Me</h1>
+                <p>I am a diligent Frontend Web Developer with years of experience in business applications. I'm eager to join your crew to build out-of-the-box solutions for regional clients. I am skilled in developing plans, managing projects, and user documentation.</p>
+            </div>
+            <div className="about-right-section">
+                <img src={AboutImage} alt="HeroImage" />
+            </div>
+            
         </div>
     )
 }
