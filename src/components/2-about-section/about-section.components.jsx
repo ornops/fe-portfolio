a49b4b9d3d302624220823fd0ddd3a72a53abc65
ornops <joshuaornopia@gmail.com>
2024-06-03 15:10:11 +0800

    Updated right side of About section

--- src/components/2-about-section/about-section.components.jsx
+++ src/components/2-about-section/about-section.components.jsx
@@ -6,8 +6,13 @@ const AboutSection = () =>{
     return(
         <div className="section-container about-section">
             <div className="about-left-section">
+                <div className="text-content-container">
                 <h1>About Me</h1>
-                <p>I am a diligent Frontend Web Developer with years of experience in business applications. I'm eager to join your crew to build out-of-the-box solutions for regional clients. I am skilled in developing plans, managing projects, and user documentation.</p>
+                <p>I am a diligent Frontend Web Developer with years of experience in business applications. 
+                    I'm eager to join your crew to build out-of-the-box solutions for regional clients. 
+                    I am skilled in developing plans, managing projects, and user documentation.</p>
+                </div>
+                
             </div>
             <div className="about-right-section">
                 <img src={AboutImage} alt="HeroImage" />
