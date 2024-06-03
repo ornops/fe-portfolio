cddf3cc552bbfd69c0b958596469859559057642
ornops <joshuaornopia@gmail.com>
2024-06-03 17:09:04 +0800

    Updated Experience Section

--- src/components/3-experience-section/experience-section.components.jsx
+++ src/components/3-experience-section/experience-section.components.jsx
@@ -4,7 +4,47 @@ const ExperienceSection = () =>{
 
     return(
         <div className="section-container experience-section">
-            <h1>Experience Section</h1>
+            <div className="experience-left-section">
+                <h1>Experience & Education</h1>
+            </div>
+            <div className="experience-right-section">
+                <div className="individual-experience-container">
+                    <div className="experience-position">
+                        <p>WORDPRESS DEVELOPER</p>
+                    </div>
+                    <div className="experience-company-name">
+                        <p>WORDPRESS CREATIVES</p>
+                    </div>
+                    <div className="experience-job-details">
+                        <p>- Build Wordpress sites based on client specifications</p>
+                    </div>
+                </div>
+
+                <div className="individual-experience-container">
+                    <div className="experience-position">
+                        <p>SOFTWARE FRONTEND ENGINEER</p>
+                    </div>
+                    <div className="experience-company-name">
+                        <p>APPSTONE</p>
+                    </div>
+                    <div className="experience-job-details">
+                        <p>- Created Web and Mobile Applications.</p>
+                    </div>
+                </div>
+
+                <div className="individual-experience-container">
+                    <div className="experience-position">
+                        <p>Senior QA Engineer for 6+ Years</p>
+                    </div>
+                    <div className="experience-company-name">
+                        <p>Mashup Garage, Outliant, Huemor, Dynata and Lexmark</p>
+                    </div>
+                    <div className="experience-job-details">
+                        <p>- Executed manual and automated testing to accurately identify bugs</p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
