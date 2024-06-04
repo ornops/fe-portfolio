e9cad9463608eb2393227d9321c9953cb2b68893
ornops <joshuaornopia@gmail.com>
2024-06-05 00:42:19 +0800

    Updated sample links

--- src/components/5-sample-section/sample-section.components.jsx
+++ src/components/5-sample-section/sample-section.components.jsx
@@ -11,17 +11,45 @@ const SampleSection = () =>{
                 <p>Sample Works</p>
             </div>
             <div className="image-gallery">
-                <div className="individual-sample-container">
-                    <img src={WorkSample1} alt="worksample1" />
-                </div>
-                <div className="individual-sample-container">
-                    <img src={WorkSample2} alt="worksample2" />
-                </div>
+                
+                    <div className="individual-sample-container">
+                        <a href="https://ornops.github.io/react-apologia-website/" target="_blank" rel="noopener noreferrer">
+                        <img src={WorkSample1} alt="worksample1" />
+                        <div className="sample-description">
+                            <p>Church Website</p>
+                        </div>
+                        </a>
+                    </div>
+                    <div className="individual-sample-container">
+                        <a href="https://ornops.github.io/react-functional-component/" target="_blank" rel="noopener noreferrer">
+                        <img src={WorkSample2} alt="worksample1" />
+                        <div className="sample-description">
+                            <p>React Search Bar</p>
+                        </div>
+                        </a>
+                    </div>
+                    <div className="individual-sample-container">
+                        <a href="https://ornops.github.io/space-invader-javascript/" target="_blank" rel="noopener noreferrer">
+                        <img src={WorkSample3} alt="worksample1" />
+                        <div className="sample-description">
+                            <p>Space Invaders</p>
+                        </div>
+                        </a>
+                    </div>
+    
+                    {/* <div className="individual-sample-container">
+                        <img src={WorkSample2} alt="worksample2" />
+                        <div className="sample-description">
+                            <p>Monsters Academy</p>
+                        </div>
+                    </div>
+
                 <div className="individual-sample-container">
                     <img src={WorkSample3} alt="worksample3" />
-                </div>
-                
-                
+                    <div className="sample-description">
+                        <p>Space Invader Game</p>
+                    </div>
+                </div> */}
                 
             </div>
         </div>
