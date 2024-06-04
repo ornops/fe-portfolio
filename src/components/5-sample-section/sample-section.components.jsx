563941464fe9bba0f52053085493248bcd6a9685
ornops <joshuaornopia@gmail.com>
2024-06-05 00:16:31 +0800

    Updates Sample Works Section

--- src/components/5-sample-section/sample-section.components.jsx
+++ src/components/5-sample-section/sample-section.components.jsx
@@ -1,10 +1,29 @@
 import './sample-section.styles.css'
+import WorkSample1 from '../../assets/sample-work-1.png'
+import WorkSample2 from '../../assets/sample-work-2.png'
+import WorkSample3 from '../../assets/sample-work-3.png'
 
 const SampleSection = () =>{
 
     return(
         <div className="section-container sample-section">
-            <h1>Sample Works Section</h1>
+            <div className="sample-works-header">
+                <p>Sample Works</p>
+            </div>
+            <div className="image-gallery">
+                <div className="individual-sample-container">
+                    <img src={WorkSample1} alt="worksample1" />
+                </div>
+                <div className="individual-sample-container">
+                    <img src={WorkSample2} alt="worksample2" />
+                </div>
+                <div className="individual-sample-container">
+                    <img src={WorkSample3} alt="worksample3" />
+                </div>
+                
+                
+                
+            </div>
         </div>
     )
 }
