a08801b14b58ff991aa101cb09027aef3a974e10
ornops <joshuaornopia@gmail.com>
2024-06-04 14:09:37 +0800

    Updated Skills Section

--- src/components/4-skills-section/skills-section.components.jsx
+++ src/components/4-skills-section/skills-section.components.jsx
@@ -1,10 +1,36 @@
 import './skills-section.styles.css'
+import SkillsImage from '../../assets/skills-image.jpg'
 
 const SkillsSection = () =>{
 
     return(
         <div className="section-container skills-section">
-            <h1>Skills Section</h1>
+            <div className="skills-left-section">
+                <p className='skills-section-header'>Skills & </p>
+                <p className='skills-section-header'>Proficiencies</p>
+                
+                <div className="skills-image-container">
+                    <img src={SkillsImage} alt="skillsImage"/>
+                </div>
+                
+            </div>
+            <div className="skills-right-section">
+                <div className="frontend-skills skills-category-holder">
+                    <p className='dev-skills-header'>DEVELOPMENT</p>
+                    <p className='skills-list'>- HTML5</p>
+                    <p className='skills-list'>- CSS3</p>
+                    <p className='skills-list'>- Javascript</p>
+                    <p className='skills-list'>- React.js</p>
+                    <p className='skills-list'>- SASS</p>
+                </div>
+                <div className="qa-skills skills-category-holder">
+                    <p className='dev-skills-header'>QA ENGINEERING</p>
+                    <p className='skills-list'>- Automation Testing</p>
+                    <p className='skills-list'>- Manual Testing</p>
+                    <p className='skills-list'>- Test Case Creation</p>
+                    <p className='skills-list'>- Bug Reporting & Debugging</p>
+                </div>
+            </div>
         </div>
     )
 }
