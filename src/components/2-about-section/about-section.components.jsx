9794d06caeef60608322e4a9915160e87b64f40c
ornops <joshuaornopia@gmail.com>
2024-06-07 17:36:55 +0800

    Updated readability in About paragraph

--- src/components/2-about-section/about-section.components.jsx
+++ src/components/2-about-section/about-section.components.jsx
@@ -8,8 +8,12 @@ const AboutSection = () =>{
             <div className="about-left-section">
                 <div className="text-content-container">
                 <h1>About Me</h1>
-                <p>Welcome to my corner of the digital universe! I'm Joshua Ornopia, a dedicated frontend developer with a knack for crafting visually stunning and user-friendly web applications. 
-                    With years of hands-on experience in frontend development, I thrive on the creative challenge of turning concepts into interactive digital solutions that captivate audiences and drive engagement. 
+                <p>Welcome to my corner of the Digital Universe!</p> 
+                    <p>I'm Joshua Ornopia, a dedicated Frontend Web Developer with a knack for crafting visually stunning and user-friendly web applications. 
+                    With years of hands-on experience in frontend development</p>
+                    <p>I thrive on the creative challenge of turning concepts into interactive digital solutions that captivate audiences and drive engagement. 
+                    </p>
+                    <p>
                     I also have more than 6 years experience as a Software QA Engineer.
                     Accomplished Quality Engineer skilled in leading testing practices in fast-paced commercial environments.</p>
                 </div>
