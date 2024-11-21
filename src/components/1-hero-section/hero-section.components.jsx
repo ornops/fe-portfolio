b98e9d27f76df919f3f426ca964b3c00872368f6
ornops <joshuaornopia@gmail.com>
2024-11-21 14:46:03 +0800

    Updated Hero and About section

--- src/components/1-hero-section/hero-section.components.jsx
+++ src/components/1-hero-section/hero-section.components.jsx
@@ -2,24 +2,24 @@ import './hero-section.styles.css'
 import HeroImage from '../../assets/hero-image.jpg'
 import TypeWriter from '../typewriter'
 
-const HeroSection = () =>{
+const HeroSection = () => {
 
-    return(
+    return (
         <div className="section-container hero-section">
             <div className="left-side-container">
-                <img src={HeroImage} alt="HeroImage" />
-            </div> 
+                <div className="image-container">
+                    <img src={HeroImage} alt="HeroImage" />
+                </div>
+
+            </div>
             <div className="right-side-container">
-                
                 <h1>
-                <TypeWriter words="Joshua Ornopia" myClass="my-name"/>
+                    <TypeWriter words="Joshua Ornopia" myClass="my-name" />
                 </h1>
                 <div>
-                    <TypeWriter words="React Developer | QA Engineer" myClass="position-title"/>
+                    <TypeWriter words="React Developer | QA Engineer" myClass="position-title" />
                 </div>
                 <a href='#contact-section'><button className="contact-button" href>Reach Out</button></a>
-                
-
             </div>
         </div>
     )
